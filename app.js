d2e54bacea8916316f011378bbcb344a24be9451
Max Oliveira <max-oliveira@outlook.com.br>
2022-12-30 18:01:55 -0300

    feat: adicionando tamplate engine ejs'

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 3000;
 const mongoose = require("mongoose");
 const linkRoute = require('./routes/linkRoute')
 
+const path = require('path');
+
 mongoose.set('strictQuery', true)
 mongoose.connect("mongodb://localhost/newlinks", {
     useNewUrlParser: true,
@@ -18,7 +20,7 @@ db.on("error", () => { console.log("Houver um erro") })
 db.once("open", () => { console.log("Banco carregado") })
 
 app.set('view engine', 'ejs');
-app.set('views')
+app.set('views',path.join(__dirname,'templates'))
 
 app.use('/', linkRoute);
 
